77403d9f29674abaae5f328ca90e7cd025dadf12
Emanuele Pavan <emanuele.pav@gmail.com>
2024-02-01 16:06:24 +0100

    creates convex schema and create board mutation

--- app/(dashboard)/_components/emptyBoards.tsx
+++ app/(dashboard)/_components/emptyBoards.tsx
@@ -2,8 +2,23 @@
 
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { useOrganization } from "@clerk/nextjs";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
 
 export const EmptyBoards = () => {
+  const { organization } = useOrganization();
+  const { mutate, pending } = useApiMutation(api.board.create);
+
+  const onClick = () => {
+    if (!organization) return;
+
+    mutate({
+      orgId: organization.id,
+      title: "untitled",
+    });
+  };
+
   return (
     <div className="h-full flex flex-col items-center justify-center">
       <Image src="/note.svg" height={110} width={110} alt="Empty" />
@@ -12,7 +27,9 @@ export const EmptyBoards = () => {
         Start by creating a board for your organization
       </p>
       <div className="mt-6">
-        <Button size="lg">Create board</Button>
+        <Button size="lg" onClick={onClick} disabled={pending}>
+          Create board
+        </Button>
       </div>
     </div>
   );
